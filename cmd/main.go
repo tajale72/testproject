b4775e823fd2d8a337ac7e8e671a163607fc40a6
Tajale72 <Tajale72@gmail.com>
2021-03-15 14:55:54 -0500

    initial commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+//SwaggerInput is a struct or
+type SwaggerInput struct {
+	FName string
+	LName string
+}
+
+//swaggerAPIis a function.
+func swaggerAPI(c *gin.Context) {
+	//Creating an object for SwaggerInput struct
+	x := SwaggerInput{"romit", "tajale"}
+
+	c.JSON(http.StatusOK, gin.H{"data": x})
+}
 
 func main() {
-	fmt.Println("New Project")
+	//Creating a gin router
+	r := gin.Default()
+
+	//GET method for swagger endpoint.
+	r.GET("/swagger", swaggerAPI)
+
+	//This will run in port : 8080
+	r.Run()
 }
