bb8e6b7e1f64bb2d135c9f20f4d4812a980684bd
krischal lageju <krischallageju@krischals-MacBook-Pro.local>
2021-05-24 18:08:46 -0500

    created new file

---
+++ cmd/gin.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/hello", Hello)
+	r.POST("/hello", Hey)
+	r.Run()
+
+}
+
+func Hello(c *gin.Context) {
+
+	var hi Hi
+	hi.Name = []string{"Krischal", "romit"}
+	hi.Adress = "413 village dr apt 120"
+	hi.Number = 6127073040
+	c.JSONP(200, hi)
+}
+
+type Hi struct {
+	Name   []string
+	Adress string
+	Number int
+}
+
+func Hey(c *gin.Context) {
+	var hi Hi
+	hi.Name = []string{"Romit", "Krischal"}
+	hi.Adress = "413 village dr apt 120"
+	hi.Number = 6127073040
+	c.JSON(200, hi)
+}
